56ff63e4aee262777d5db13943c088ad16390a59
stories2 <stories2@naver.com>
2018-01-03 08:23:26 +0900

    Make basic kakao method

--- functions/index.js
+++ functions/index.js
@@ -9,8 +9,45 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 
 exports.keyboard = functions.https.onRequest((request, response) => {
 
-    responseMessage = {"type" : "buttons", "buttons" : ["선택 1", "선택 2", "선택 3"]}
+    responseMessage = {"type" : "buttons", "buttons" : ["곧 떠날 달구지", "전체 달구지 시간표", "서비스 정보"]}
 
- response.setHeader('Content-Type', 'application/json');
+ // response.setHeader('Content-Type', 'application/json');
  response.status(200).send(JSON.stringify(responseMessage))
+});
+
+exports.message = functions.https.onRequest((request, response) => {
+    userContent = ""
+    switch(request.method) {
+        case 'POST':
+            requestMessage = request.body
+            userContent = requestMessage["content"]
+            break;
+        default:
+            break;
+    }
+
+    responseMessage = {"message": {"text": "selection: " + userContent}, "keyboard": {"type" : "buttons", "buttons" : ["선택 1", "선택 2", "선택 3"]}}
+
+    response.setHeader('Content-Type', 'application/json');
+    response.status(200).send(JSON.stringify(responseMessage))
+});
+
+exports.friend = functions.https.onRequest((request, response) => {
+    switch(request.method) {
+        case 'POST':
+            break;
+        case 'DELETE':
+            break;
+        default:
+            break;
+    }
+});
+
+exports.chat_room = functions.https.onRequest((request, response) => {
+    switch(request.method) {
+        case 'DELETE':
+            break;
+        default:
+            break;
+    }
 });
