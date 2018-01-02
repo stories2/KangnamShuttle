99a51ea4108fddb9b3e4c4b0ac1de7cfa531e81a
stories2 <stories2@naver.com>
2018-01-03 08:38:34 +0900

    Make main loop solution

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,19 @@
 const functions = require('firebase-functions');
 
+var LEAVE_AS_SOON_AS_SHUTTLE = "곧 떠날 달구지"
+var ALL_SHUTTLE_TIME = "전체 달구지 시간표"
+var SERVICE_INFO = "서비스 정보"
+var GIHEUNG_TO_SCHOOL = "기흥역 -> 강남대 이공관"
+var KANGNAM_UNIV_STATION_TO_SCHOOL = "강남대역 -> 강남대 이공관"
+var SCHOOL_TO_GIHEUNG = "강남대 이공관 -> 기흥역"
+var SCHOOL_TO_KANGNAM_UNIV_STATION = "강남대 이공관 -> 강남대역"
+var MAIN_BUTTONS = [LEAVE_AS_SOON_AS_SHUTTLE, ALL_SHUTTLE_TIME, SERVICE_INFO]
+var SHUTTLE_START_POINT_BUTTONS = [
+    GIHEUNG_TO_SCHOOL,
+    KANGNAM_UNIV_STATION_TO_SCHOOL,
+    SCHOOL_TO_GIHEUNG,
+    SCHOOL_TO_KANGNAM_UNIV_STATION]
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -9,7 +23,7 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 
 exports.keyboard = functions.https.onRequest((request, response) => {
 
-    responseMessage = {"type" : "buttons", "buttons" : ["곧 떠날 달구지", "전체 달구지 시간표", "서비스 정보"]}
+    responseMessage = {"type" : "buttons", "buttons" : MAIN_BUTTONS}
 
  // response.setHeader('Content-Type', 'application/json');
  response.status(200).send(JSON.stringify(responseMessage))
@@ -17,16 +31,23 @@ exports.keyboard = functions.https.onRequest((request, response) => {
 
 exports.message = functions.https.onRequest((request, response) => {
     userContent = ""
+    responseButton = []
     switch(request.method) {
         case 'POST':
             requestMessage = request.body
             userContent = requestMessage["content"]
+            if(userContent == LEAVE_AS_SOON_AS_SHUTTLE) {
+                responseButton = SHUTTLE_START_POINT_BUTTONS
+            }
+            else {
+                responseButton = MAIN_BUTTONS
+            }
             break;
         default:
             break;
     }
 
-    responseMessage = {"message": {"text": "selection: " + userContent}, "keyboard": {"type" : "buttons", "buttons" : ["선택 1", "선택 2", "선택 3"]}}
+    responseMessage = {"message": {"text": "selection: " + userContent}, "keyboard": {"type" : "buttons", "buttons" : responseButton}}
 
     response.setHeader('Content-Type', 'application/json');
     response.status(200).send(JSON.stringify(responseMessage))
