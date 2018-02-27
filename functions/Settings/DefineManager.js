1eb9b1a70ea90595381f294c021e73533c37ee47
Kim HyunWoo <stories2@naver.com>
2018-02-27 16:55:53 +0900

    DefineManager module : remove hardcoded data from index module

---
+++ functions/Settings/DefineManager.js
@@ -0,0 +1,19 @@
+exports.LEAVE_AS_SOON_AS_SHUTTLE = "곧 떠날 달구지를 알려줘"
+exports.ALL_SHUTTLE_TIME = "전체 달구지 시간표를 알려줘"
+exports.SERVICE_INFO = "서비스 정보"
+exports.GIHEUNG_TO_SCHOOL = "기흥역에서 이공관으로 갈꺼야"
+exports.KANGNAM_UNIV_STATION_TO_SCHOOL = "강남대역에서 이공관으로 갈꺼야"
+exports.SCHOOL_TO_GIHEUNG = "이공관에서 기흥역으로 갈꺼야"
+exports.SCHOOL_TO_KANGNAM_UNIV_STATION = "이공관에서 강남대역으로 갈꺼야"
+exports.MAIN_BUTTONS = [exports.LEAVE_AS_SOON_AS_SHUTTLE, exports.ALL_SHUTTLE_TIME, exports.SERVICE_INFO]
+exports.SHUTTLE_START_POINT_BUTTONS = [
+    exports.GIHEUNG_TO_SCHOOL,
+    exports.KANGNAM_UNIV_STATION_TO_SCHOOL,
+    exports.SCHOOL_TO_GIHEUNG,
+    exports.SCHOOL_TO_KANGNAM_UNIV_STATION]
+exports.DATABASE_GIHEUNG_TO_SCHOOL = "GiheungToSchool"
+exports.DATABASE_KANGNAM_UNIV_TO_SCHOOL = "KangnamUnivToSchool"
+exports.DATABASE_SCHOOL_TO_GIHEUNG = "SchoolToGiheung"
+exports.DATABASE_SCHOOL_TO_KANGNAM_UNIV = "SchoolToKangnamUniv"
+exports.STORAGE_SHARE_CARD_URL = "https://firebasestorage.googleapis.com/v0/b/kangnamshuttle.appspot.com/o/poster.png?alt=media&token=fb60f794-50a2-40c8-be0c-f2400eabaad4"
+exports.PLZ_INPUT_DEPART_AND_ARRIVE_POINT = "출발 지점과 도착지점을 알려주세요!"
--- functions/index.js
+++ functions/index.js
@@ -5,20 +5,6 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-var LEAVE_AS_SOON_AS_SHUTTLE = "곧 떠날 달구지를 알려줘"
-var ALL_SHUTTLE_TIME = "전체 달구지 시간표를 알려줘"
-var SERVICE_INFO = "서비스 정보"
-var GIHEUNG_TO_SCHOOL = "기흥역에서 이공관으로 갈꺼야"
-var KANGNAM_UNIV_STATION_TO_SCHOOL = "강남대역에서 이공관으로 갈꺼야"
-var SCHOOL_TO_GIHEUNG = "이공관에서 기흥역으로 갈꺼야"
-var SCHOOL_TO_KANGNAM_UNIV_STATION = "이공관에서 강남대역으로 갈꺼야"
-var MAIN_BUTTONS = [LEAVE_AS_SOON_AS_SHUTTLE, ALL_SHUTTLE_TIME, SERVICE_INFO]
-var SHUTTLE_START_POINT_BUTTONS = [
-    GIHEUNG_TO_SCHOOL,
-    KANGNAM_UNIV_STATION_TO_SCHOOL,
-    SCHOOL_TO_GIHEUNG,
-    SCHOOL_TO_KANGNAM_UNIV_STATION]
-
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -28,7 +14,7 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 
 exports.keyboard = functions.https.onRequest((request, response) => {
 
-    responseMessage = {"type" : "buttons", "buttons" : MAIN_BUTTONS}
+    responseMessage = {"type" : "buttons", "buttons" : global.defineManager.MAIN_BUTTONS}
 
  // response.setHeader('Content-Type', 'application/json');
  response.status(200).send(JSON.stringify(responseMessage))
@@ -51,44 +37,44 @@ exports.message = functions.https.onRequest((request, response) => {
                 requestMessage = request.body
                 userContent = requestMessage["content"]
                 // console.log("content: " + userContent)
-                if(userContent == LEAVE_AS_SOON_AS_SHUTTLE) {
+                if(userContent == global.defineManager.LEAVE_AS_SOON_AS_SHUTTLE) {
                     // console.log("selection: leave soon, all")
-                    responseButton = SHUTTLE_START_POINT_BUTTONS
-                    responseText = "출발 지점과 도착지점을 알려주세요!"
+                    responseButton = global.defineManager.SHUTTLE_START_POINT_BUTTONS
+                    responseText = global.defineManager.PLZ_INPUT_DEPART_AND_ARRIVE_POINT
                     responseMessage["text"] = responseText
                 }
-                else if(userContent == ALL_SHUTTLE_TIME) {
-                    responseButton = MAIN_BUTTONS
+                else if(userContent == global.defineManager.ALL_SHUTTLE_TIME) {
+                    responseButton = global.defineManager.MAIN_BUTTONS
                     responseText = PrintAllShuttle(userContent, databaseSnapshot)
                     responseMessage["text"] = responseText
                 }
-                else if(userContent == SERVICE_INFO) {
+                else if(userContent == global.defineManager.SERVICE_INFO) {
                     // console.log("selection: service info")
-                    responseButton = MAIN_BUTTONS
+                    responseButton = global.defineManager.MAIN_BUTTONS
                     // system
                     systemData = databaseSnapshot["System"]
                     responseText = "버전: " + systemData["ver"] + "\n최종 수정일: " + systemData["lastEdit"] +
                         "\n개발자: " + systemData["developer"] + "\n메일: " + systemData["email"]
 
-                    labelButton = {"label": "공유하기", "url": "https://firebasestorage.googleapis.com/v0/b/kangnamshuttle.appspot.com/o/poster.png?alt=media&token=fb60f794-50a2-40c8-be0c-f2400eabaad4"}
+                    labelButton = {"label": "공유하기", "url": global.defineManager.STORAGE_SHARE_CARD_URL}
                     responseMessage["message_button"] = labelButton
                     responseMessage["text"] = responseText
                 }
                 else {
                     // console.log("selection: else")
-                    responseButton = MAIN_BUTTONS
+                    responseButton = global.defineManager.MAIN_BUTTONS
                     switch (userContent){
-                        case GIHEUNG_TO_SCHOOL:
-                            responseText = PrintFastestShuttle(userContent, databaseSnapshot["GiheungToSchool"])
+                        case global.defineManager.GIHEUNG_TO_SCHOOL:
+                            responseText = PrintFastestShuttle(userContent, databaseSnapshot[global.defineManager.DATABASE_GIHEUNG_TO_SCHOOL])
                             break;
-                        case KANGNAM_UNIV_STATION_TO_SCHOOL:
-                            responseText = PrintFastestShuttle(userContent, databaseSnapshot["KangnamUnivToSchool"])
+                        case global.defineManager.KANGNAM_UNIV_STATION_TO_SCHOOL:
+                            responseText = PrintFastestShuttle(userContent, databaseSnapshot[global.defineManager.DATABASE_KANGNAM_UNIV_TO_SCHOOL])
                             break;
-                        case SCHOOL_TO_GIHEUNG:
-                            responseText = PrintFastestShuttle(userContent, databaseSnapshot["SchoolToGiheung"])
+                        case global.defineManager.SCHOOL_TO_GIHEUNG:
+                            responseText = PrintFastestShuttle(userContent, databaseSnapshot[global.defineManager.DATABASE_SCHOOL_TO_GIHEUNG])
                             break;
-                        case SCHOOL_TO_KANGNAM_UNIV_STATION:
-                            responseText = PrintFastestShuttle(userContent, databaseSnapshot["SchoolToKangnamUniv"])
+                        case global.defineManager.SCHOOL_TO_KANGNAM_UNIV_STATION:
+                            responseText = PrintFastestShuttle(userContent, databaseSnapshot[global.defineManager.DATABASE_SCHOOL_TO_KANGNAM_UNIV])
                             break;
                         default:
                             responseText = "selection: " + userContent
