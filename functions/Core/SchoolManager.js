28a06e04bfef48404d45cb83e0378e90df4c8a00
Kim HyunWoo <stories2@naver.com>
2018-03-18 18:18:22 +0900

    SchoolManager module : delete useless request

--- functions/Core/SchoolManager.js
+++ functions/Core/SchoolManager.js
@@ -19,99 +19,53 @@ exports.GetAcademicScheduleThisMonth = function (month, admin, convertManager, g
             "Upgrade-Insecure-Requests": "1",
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
             "Referer": "http://web.kangnam.ac.kr/",
-            "Accept-Language": "en-US,en;q=0.9,ko;q=0.8"
+            "Accept-Language": "en-US,en;q=0.9,ko;q=0.8",
+            // "Cookie": sessionId
         }
     }
-
-    httpManager.get(httpHeadersOptions, function(res) {
-        // console.log('statusCode:', res.statusCode);
-        // console.log('headers:', res.headers);
-        // console.log('headers:', res.headers["set-cookie"]);
+    // console.log("http rerequest: ", httpHeadersOptions)
+    httpManager.get(httpHeadersOptions, function (response) {
+        // console.log('statusCode:', response.statusCode);
+        // console.log('headers:', response.headers);
 
         global.logManager.PrintLogMessage("SchoolManager", "GetAcademicScheduleThisMonth",
-            "index.do getting http statusCode: " + res.statusCode + " headers: " + res.headers, global.defineManager.LOG_LEVEL_DEBUG)
-
-        sessionId = res.headers["set-cookie"][0].split(";")[0]
-        // console.log('headers:', sessionId);
-
-        res.on('data', function (chunk) {
-            // console.log("http data: " + chunk)
+            "schedule.do getting http statusCode: " + response.statusCode + " headers: " + response.headers, global.defineManager.LOG_LEVEL_DEBUG)
+        var serverData = ''
+        response.on('data', function (chunk) {
+            serverData += chunk
         })
-
-        res.on('end', function () {
+        response.on('end', function () {
+            var $ = cheerioManager.load(serverData)
             global.logManager.PrintLogMessage("SchoolManager", "GetAcademicScheduleThisMonth",
-                "index.do getting http session ok: " + sessionId, global.defineManager.LOG_LEVEL_INFO)
+                "http target url requested ok", global.defineManager.LOG_LEVEL_INFO)
 
-            cookie = requestManager.cookie(sessionId)
-            httpHeadersOptions = {
-                hostname: "web.kangnam.ac.kr",
-                path: "/menu/02be162adc07170ec7ee034097d627e9.do",
-                method: "GET",
-                headers: {
-                    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.162 Safari/537.36",
-                    'Content-Type': 'text/html',
-                    "Connection": "keep-alive",
-                    "Pragma": "no-cache",
-                    "Cache-Control": "no-cache",
-                    "Upgrade-Insecure-Requests": "1",
-                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-                    "Referer": "http://web.kangnam.ac.kr/",
-                    "Accept-Language": "en-US,en;q=0.9,ko;q=0.8",
-                    "Cookie": sessionId
-                }
-            }
-            // console.log("http rerequest: ", httpHeadersOptions)
-            httpManager.get(httpHeadersOptions, function (response) {
-                // console.log('statusCode:', response.statusCode);
-                // console.log('headers:', response.headers);
-
-                global.logManager.PrintLogMessage("SchoolManager", "GetAcademicScheduleThisMonth",
-                    "schedule.do getting http statusCode: " + response.statusCode + " headers: " + response.headers, global.defineManager.LOG_LEVEL_DEBUG)
-                var serverData = ''
-                response.on('data', function (chunk) {
-                    serverData += chunk
+            scheduleCalendar = $("table.cal_data tbody")
+            // console.log("raw data: ", scheduleCalendar)
+            yearSchedule = []
+            scheduleCalendar.each(function () {
+                calChildren = $(this).children("tr")
+                // console.log("month")
+                indexOfMonth = []
+                calChildren.each(function () {
+                    colText = $(this).text()
+                    // console.log("test ", colText)
+                    indexOfMonth.push(colText)
                 })
-                response.on('end', function () {
-                    var $ = cheerioManager.load(serverData)
-                    global.logManager.PrintLogMessage("SchoolManager", "GetAcademicScheduleThisMonth",
-                        "http target url requested ok", global.defineManager.LOG_LEVEL_INFO)
-
-                    scheduleCalendar = $("table.cal_data tbody")
-                    // console.log("raw data: ", scheduleCalendar)
-                    yearSchedule = []
-                    scheduleCalendar.each(function () {
-                        calChildren = $(this).children("tr")
-                        // console.log("month")
-                        indexOfMonth = []
-                        calChildren.each(function () {
-                            colText = $(this).text()
-                            // console.log("test ", colText)
-                            indexOfMonth.push(colText)
-                        })
-                        yearSchedule.push(indexOfMonth)
-                    })
-                    // console.log("year schedule: ", yearSchedule)
-                    scheduleStr = global.util.format(global.defineManager.SCHEDULE_RESULT_STR, (month + 1))
-                    scheduleStr = scheduleStr + yearSchedule[month].join("\n")
-                    responseButton = global.defineManager.MAIN_BUTTONS
-                    responseMessage["text"] = scheduleStr
-                    responseManager.TemplateResponse(admin, convertManager, generateManager, responseApp, requestMessage, responseMessage, responseButton)
-                })
-                response.on('error', function (except) {
-                    global.logManager.PrintLogMessage("SchoolManager", "GetAcademicScheduleThisMonth",
-                        "http error while request target url: schedule.do\nerr: " + except, global.defineManager.LOG_LEVEL_ERROR)
-                    responseButton = global.defineManager.MAIN_BUTTONS
-                    responseMessage["text"] = global.defineManager.ERROR_MSG
-                    responseManager.TemplateResponse(admin, convertManager, generateManager, responseApp, requestMessage, responseMessage, responseButton)
-                })
+                yearSchedule.push(indexOfMonth)
             })
+            // console.log("year schedule: ", yearSchedule)
+            scheduleStr = global.util.format(global.defineManager.SCHEDULE_RESULT_STR, (month + 1))
+            scheduleStr = scheduleStr + yearSchedule[month].join("\n")
+            responseButton = global.defineManager.MAIN_BUTTONS
+            responseMessage["text"] = scheduleStr
+            responseManager.TemplateResponse(admin, convertManager, generateManager, responseApp, requestMessage, responseMessage, responseButton)
         })
-
-    }).on('error', function(exception) {
-        global.logManager.PrintLogMessage("SchoolManager", "GetAcademicScheduleThisMonth",
-            "http error while request target url: index.do\nerr: " + except, global.defineManager.LOG_LEVEL_ERROR)
-        responseButton = global.defineManager.MAIN_BUTTONS
-        responseMessage["text"] = global.defineManager.ERROR_MSG
-        responseManager.TemplateResponse(admin, convertManager, generateManager, responseApp, requestMessage, responseMessage, responseButton)
-    });
+        response.on('error', function (except) {
+            global.logManager.PrintLogMessage("SchoolManager", "GetAcademicScheduleThisMonth",
+                "http error while request target url: schedule.do\nerr: " + except, global.defineManager.LOG_LEVEL_ERROR)
+            responseButton = global.defineManager.MAIN_BUTTONS
+            responseMessage["text"] = global.defineManager.ERROR_MSG
+            responseManager.TemplateResponse(admin, convertManager, generateManager, responseApp, requestMessage, responseMessage, responseButton)
+        })
+    })
 }
