37a7e914464d6ada4d91d44121d6f3a9895c9115
Kim HyunWoo <stories2@naver.com>
2018-03-30 23:48:35 +0900

    AdManager module : show advertise table

--- public/js/core/AdManager.js
+++ public/js/core/AdManager.js
@@ -1,14 +1,37 @@
 function AdManager() {
+    PrintLogMessage("AdManager", "AdManager", "init", LOG_LEVEL_INFO)
     dataTransferManager = new DataTransferManager()
     dataTransferManager.GetRequest(
         DOMAIN + "getListOfAdvertise",
         "",
         this)
+
+    this.tableRowList = []
 }
 
 AdManager.prototype.RequestSuccess = function (data) {
-    dataLen = data.length
+    jsonParsedData = JSON.parse(data)
+    dataLen = jsonParsedData.length
     PrintLogMessage("AdManager", "RequestSuccess", "data received successfully len: " + dataLen, LOG_LEVEL_INFO)
+    for(key in jsonParsedData) {
+        PrintLogMessage("AdManager", "RequestSuccess", "check data: " + jsonParsedData[key], LOG_LEVEL_DEBUG)
+        indexOfAd = jsonParsedData[key]
+
+        obj = $(".template-of_schedule-status").clone()
+        obj.attr("class", "indexOfRow")
+        obj.removeAttr("hidden")
+        obj.find(".adTime").text(indexOfAd["time"])
+        obj.find(".adText").text(indexOfAd["text"])
+        obj.find(".adDuration").text(indexOfAd["duration"])
+        if(indexOfAd["enable"] == ENABLE) {
+            obj.find(".adEnable").text(ACTIVE_STR)
+        }
+        else if(indexOfAd["enable"] == DISABLE) {
+            obj.find(".adEnable").text(DEACTIVE_STR)
+        }
+        this.tableRowList.push(obj.appendTo(".schedule-status-tbody"))
+    }
+
 }
 
 AdManager.prototype.RequestFail = function (responseText, status) {
--- public/js/settings/DefineManager.js
+++ public/js/settings/DefineManager.js
@@ -4,4 +4,10 @@ LOG_LEVEL_DEBUG = 2
 LOG_LEVEL_WARN = 3
 LOG_LEVEL_ERROR = 4
 
+ENABLE = 1
+DISABLE = 0
+
+ACTIVE_STR = "Active"
+DEACTIVE_STR = "Deactivate"
+
 DOMAIN = "http://localhost:5000/kangnamshuttle/us-central1/"
