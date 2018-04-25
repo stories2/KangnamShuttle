1b6bd7664c5abb897d029599d18b2d3c5fef50d5
Kim HyunWoo <stories2@naver.com>
2018-04-25 19:21:50 +0900

    <Flask> static : edit static page

--- Flask-Helper/index.py
+++ Flask-Helper/index.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/')
+def home():
+    return render_template('demo-static.html')
