cee4e15a7dab4915f4aada5042dc87acede7ad60
Kim HyunWoo <stories2@naver.com>
2018-05-08 22:58:30 +0900

    BusTrackingManager : checking am i logged in successfully

--- functions/Core/BusTrackingManager.js
+++ functions/Core/BusTrackingManager.js
@@ -44,7 +44,7 @@ exports.LoginUbikan = function (busTrackingManager, loginInfo) {
             global.logManager.PrintLogMessage("BusTrackingManager", "LoginUbikan", "response accepted: " + str,
                 global.defineManager.LOG_LEVEL_DEBUG)
 
-            busTrackingManager.CheckLoggedInUbikan(httpRequestResponse.headers)
+            busTrackingManager.CheckLoggedInUbikan(httpRequestResponse.headers["set-cookie"], busTrackingManager)
         });
         httpRequestResponse.on('error', function (except) {
             global.logManager.PrintLogMessage("BusTrackingManager", "LoginUbikan", "somthing goes wrong: " + except,
@@ -59,11 +59,83 @@ exports.LoginUbikan = function (busTrackingManager, loginInfo) {
     httpRequest.end();
 }
 
-exports.CheckLoggedInUbikan = function (ubikanLoginRequestHeaders) {
-    global.logManager.PrintLogMessage("BusTrackingManager", "CheckLoggedInUbikan", "cookie val: " + ubikanLoginRequestHeaders["set-cookie"],
+exports.CheckLoggedInUbikan = function (cookieData, busTrackingManager) {
+    var httpManager = require("http")
+
+    global.logManager.PrintLogMessage("BusTrackingManager", "CheckLoggedInUbikan", "cookie val: " + cookieData,
         global.defineManager.LOG_LEVEL_DEBUG)
+
+    fakeHeaderOptions = {
+        hostname: 'new.ubikhan.com',
+        path: '/member/check_logged',
+        port: '80',
+        method: 'POST',
+        headers: {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.162 Safari/537.36",
+            'Content-Type': 'application/x-www-form-urlencoded',
+            "Connection": "keep-alive",
+            "Pragma": "no-cache",
+            "Cache-Control": "no-cache",
+            "Upgrade-Insecure-Requests": "1",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.9,ko;q=0.8",
+            "Cookie": cookieData
+        }
+    }
+
+    httpRequestCallback = function(httpRequestResponse) {
+        global.logManager.PrintLogMessage("BusTrackingManager", "CheckLoggedInUbikan",
+            "status code: " + httpRequestResponse.statusCode + " headers: " + httpRequestResponse.headers, global.defineManager.LOG_LEVEL_INFO)
+
+        var str = ''
+        httpRequestResponse.on('data', function (chunk) {
+            str += chunk;
+        });
+
+        httpRequestResponse.on('end', function () {
+            // console.log(str);
+            global.logManager.PrintLogMessage("BusTrackingManager", "CheckLoggedInUbikan", "response accepted: " + str,
+                global.defineManager.LOG_LEVEL_DEBUG)
+
+            ubikanLoginResponseData = JSON.parse(str)
+
+            if(ubikanLoginResponseData["Logged In"] == true) {
+                global.logManager.PrintLogMessage("BusTrackingManager", "CheckLoggedInUbikan", "successfully logged in",
+                    global.defineManager.LOG_LEVEL_DEBUG)
+            }
+        });
+        httpRequestResponse.on('error', function (except) {
+            global.logManager.PrintLogMessage("BusTrackingManager", "CheckLoggedInUbikan", "somthing goes wrong: " + except,
+                global.defineManager.LOG_LEVEL_ERROR)
+        })
+    }
+
+    var httpRequest = httpManager.request(fakeHeaderOptions, httpRequestCallback);
+    httpRequest.end();
 }
 
-exports.GetBusDataList = function () {
+exports.GetBusDataList = function (cookieData, busTrackingManager) {
 
+
+    // fakeHeaderOptions = {
+    //     hostname: 'new.ubikhan.com',
+    //     path: '/member/login',
+    //     port: '80',
+    //     method: 'POST',
+    //     // referer: "http://new.ubikhan.com/member/login?request_url=map",
+    //     Referer: "http://new.ubikhan.com/map",
+    //     headers: {
+    //         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.162 Safari/537.36",
+    //         'Content-Type': 'application/x-www-form-urlencoded',
+    //         'Content-Length': Buffer.byteLength(postData,'utf8'),
+    //         'Referer': "http://new.ubikhan.com/map",
+    //         "Connection": "keep-alive",
+    //         "Pragma": "no-cache",
+    //         "Cache-Control": "no-cache",
+    //         "Upgrade-Insecure-Requests": "1",
+    //         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+    //         "Accept-Language": "en-US,en;q=0.9,ko;q=0.8",
+    //         "Cookie": cookieData
+    //     }
+    // }
 }
