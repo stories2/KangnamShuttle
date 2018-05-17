a4dec99975112affd3f122944ba41476b3b12a20
Kim HyunWoo <stories2@naver.com>
2018-05-18 00:36:07 +0900

    Hosting : <MapViewManager> control view manager added

---
+++ public/js/core/MapManager.js
@@ -0,0 +1,27 @@
+function MapManager() {
+    
+}
+
+MapManager.prototype.GetBusLocation = function () {
+    
+}
+
+MapManager.prototype.GetBusLocationOk = function (data) {
+    
+}
+
+MapManager.prototype.GetBusLocationFail = function (errorText, errorStatus) {
+    
+}
+
+MapManager.prototype.UpdateBusLocation = function () {
+    
+}
+
+MapManager.prototype.UpdateBusLocationOk = function (data) {
+    
+}
+
+MapManager.prototype.UpdateBusLocationFail = function (errorText, errorStatus) {
+    
+}
