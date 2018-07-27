15b5cc3fca79b7868ca0f36f692ccee113a73dcd
Kim HyunWoo <stories2@naver.com>
2018-07-27 23:53:44 +0900

    Hosting <BigTemplateMan> : include firebase init script

--- functions/Public/js/utils/FirebaseInit.js
+++ functions/Public/js/utils/FirebaseInit.js
@@ -1,13 +1,10 @@
-function FirebaseInit() {
-
-    // Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyBv58DW58VFaQjKTJvRmX2izLcRqrbsbVI",
-        authDomain: "kangnamshuttle.firebaseapp.com",
-        databaseURL: "https://kangnamshuttle.firebaseio.com",
-        projectId: "kangnamshuttle",
-        storageBucket: "kangnamshuttle.appspot.com",
-        messagingSenderId: "356849424734"
-    };
-    firebase.initializeApp(config);
-}
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBv58DW58VFaQjKTJvRmX2izLcRqrbsbVI",
+    authDomain: "kangnamshuttle.firebaseapp.com",
+    databaseURL: "https://kangnamshuttle.firebaseio.com",
+    projectId: "kangnamshuttle",
+    storageBucket: "kangnamshuttle.appspot.com",
+    messagingSenderId: "356849424734"
+};
+firebase.initializeApp(config);
