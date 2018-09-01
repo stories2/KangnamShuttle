3e1b98a387be8c50f9e6b222d6f020dc7e3c90a7
Kim HyunWoo <stories2@naver.com>
2018-09-01 14:29:35 +0900

    BusManager: get bus location data from open api endpoint

--- functions/Core/BusManager.js
+++ functions/Core/BusManager.js
@@ -15,5 +15,66 @@ exports.GetAllBusStation = function (admin, responseText, callbackFunc) {
 }
 
 exports.UpdatePublicBusLocation = function(admin, busRoutineName, openApiInfo) {
+    var httpManager = require("http");
+    var x2js = require("x2js")
     global.logManager.PrintLogMessage("BusManager", "UpdatePublicBusLocation", "update routine name: " + busRoutineName + " api info: " + JSON.stringify(openApiInfo), global.defineManager.LOG_LEVEL_DEBUG)
+
+    admin.database().ref(global.defineManager.DATABASE_SERVICE_V2_0_0_PUBLIC_BUS_STATION_PATH).once('value', function(publicBusStationListSnapshot) {
+        publicBusStationListSnapshot = JSON.parse(JSON.stringify(publicBusStationListSnapshot))
+
+        for(key in publicBusStationListSnapshot) {
+            publicBusStation = publicBusStationListSnapshot[key]
+            if(publicBusStation["id"] == openApiInfo["stationId"]) {
+                global.logManager.PrintLogMessage("BusManager", "UpdatePublicBusLocation", "debug: " + JSON.stringify(publicBusStation), global.defineManager.LOG_LEVEL_DEBUG)
+                publicBusRoutine = publicBusStation["busRoutines"][busRoutineName]
+                global.logManager.PrintLogMessage("BusManager", "UpdatePublicBusLocation", "name: " + busRoutineName + " routine: " + publicBusRoutine, global.defineManager.LOG_LEVEL_DEBUG)
+                openApiPath = global.util.format(openApiInfo["endpoint_path"], openApiInfo["key"], publicBusRoutine)
+                publicBusLocationPath = global.util.format(global.defineManager.DATABASE_SERVICE_V2_0_0_PUBLIC_BUS_LOCATION_INFO_PATH, publicBusRoutine)
+
+                fakeHeaderOptions = {
+                    hostname: openApiInfo["endpoint"],
+                    path: openApiPath,
+                }
+
+                global.logManager.PrintLogMessage("BusManager", "UpdatePublicBusLocation", "api request rdy: " + JSON.stringify(fakeHeaderOptions), global.defineManager.LOG_LEVEL_DEBUG)
+
+                httpRequestCallback = function(httpRequestResponse) {
+                    global.logManager.PrintLogMessage("BusManager", "httpRequestCallback",
+                        "status code: " + httpRequestResponse.statusCode + " headers: " + httpRequestResponse.headers, global.defineManager.LOG_LEVEL_DEBUG)
+
+                    var str = ''
+                    httpRequestResponse.on('data', function (chunk) {
+                        str += chunk;
+                    });
+
+                    httpRequestResponse.on('end', function () {
+                        // console.log(str);
+                        global.logManager.PrintLogMessage("BusManager", "httpRequestCallback", "response accepted: " + str,
+                            global.defineManager.LOG_LEVEL_DEBUG)
+
+                        busResponseInfo = x2js.xml2js(str)
+
+                        global.logManager.PrintLogMessage("BusManager", "httpRequestCallback", "bus location info save path: " + publicBusLocationPath,
+                            global.defineManager.LOG_LEVEL_DEBUG)
+
+                        admin.database().ref(publicBusLocationPath).set(busResponseInfo)
+
+                    });
+                    httpRequestResponse.on('error', function (except) {
+                        global.logManager.PrintLogMessage("BusManager", "httpRequestCallback", "somthing goes wrong: " + except,
+                            global.defineManager.LOG_LEVEL_ERROR)
+                    })
+                }
+
+                var httpRequest = httpManager.request(fakeHeaderOptions, httpRequestCallback);
+                httpRequest.end();
+
+                break;
+            }
+        }
+    })
+
+
+
+
 }
--- functions/Settings/DefineManager.js
+++ functions/Settings/DefineManager.js
@@ -191,6 +191,8 @@ exports.DATABASE_SERVICE_V2_0_0_BUS_CONFUSION_TIME_INFO_PATH = "/V2/Service/V2_0
 exports.DATABASE_SERVICE_V2_0_0_BUS_CONFUSION_TIME_STR_PATH = "/V2/Service/V2_0_0/StaticContent/BusInfo/ConfusionTimeInfoStr/%s"
 exports.DATABASE_SERVICE_V2_0_0_PUBLIC_BUS_KANGNAM_UNIV_ROUTINE_PATH = "/V2/Service/V2_0_0/StaticContent/BusInfo/publicBusInfo/station/kangnamUnivStation"
 exports.DATABASE_SERVICE_V2_0_0_PUBLIC_BUS_NEXT_TO_KANGNAM_UNIV_ROUTINE_PATH = "/V2/Service/V2_0_0/StaticContent/BusInfo/publicBusInfo/station/nextToKangnamUnivStation"
+exports.DATABASE_SERVICE_V2_0_0_PUBLIC_BUS_STATION_PATH = "/V2/Service/V2_0_0/StaticContent/BusInfo/publicBusInfo/station"
+exports.DATABASE_SERVICE_V2_0_0_PUBLIC_BUS_LOCATION_INFO_PATH = "/V2/Service/V2_0_0/StaticContent/BusInfo/publicBusInfo/%s"
 
 exports.AUTOMATON_START_NUMBER = 1
 exports.AUTOMATON_DEFAULT_INPUT_NUMBER = 0
