7d7306adf750bdb4cacf75ed074417261326892f
Kim HyunWoo <stories2@naver.com>
2018-09-17 00:18:43 +0900

    AutomatonManager : support main info system url

--- functions/Core/AutomatonManager.js
+++ functions/Core/AutomatonManager.js
@@ -441,6 +441,13 @@ exports.OrderExecute = function (admin, functions, request, currentRoutineLinkIt
 
         busManager.UpdatePublicBusLocation(admin, request.body["content"], busOpenApiInfo)
     }
+    else if(currentOrderNumber == global.defineManager.AUTOMATON_MAIN_INFO_SYSTEM_ORDER_NUMBER) {
+        makeUpResponse(
+            currentRoutineLinkItem["responseMsgDic"][currentUserResponseMsgType][global.defineManager.RESPONSE_DEFAULT_SELECTION],
+            currentRoutineLinkItem["labelUrl"],
+            null
+        )
+    }
     // else if(currentOrderNumber == global.defineManager.AUTOMATON_PUBLIC_TRANSPORT_INFO_ORDER_NUMBER) {
     //
     //     busOpenApiInfo = {
